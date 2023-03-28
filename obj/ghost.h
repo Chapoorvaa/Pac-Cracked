fb8c4c03951b7ee228f15af43a831f5648fc555a
Arthur Gailleul <arthur.gailleul@epita.fr>
2023-03-28 13:39:16 +0200

    remoddled ghost.h

--- obj/ghost.h
+++ obj/ghost.h
@@ -58,24 +58,17 @@ typedef struct Ghosts
 void ghostInit(struct Ghost* ghost, char* label, int x, int y, int speed,
  int direction, int mode, int scatterTargetX, int scatterTargetY, int spawnX, int spawnY);
 
-// Check if the ghost is in a intersection and act accordingly
-// 1. if it's in a intersection, call ghostPathing
-// 2. if it's not in a intersection, and the next position is a wall,
-//      change the direction to where the only direction it can go, update the next position + direction
-// 3. if it's not in a intersection move the ghost
-void ghostMove(struct Ghost* ghost, char** map, struct Player* player);
+// Moves the ghost and checks for collisions + intersections with the map
+void GhostMove(struct Ghost* ghost, char** map, struct Player* player);
 
+// Just moves the ghost (however no checks are made)
+void move(Ghost* ghost);
 
-// This function will use the A* algorithm to find the shortest path to the target position
-void Astar(struct Ghost* ghost, char** map);
+// Checks if there is a wall in front of the ghost and changes direction
+void checkWall(Ghost* ghost, char** map);
 
-// This function is called when a ghost is in a intersection
-// It will check the ghost's state and target it's next position
-//check for it's state:
-//  1.1 if it's in scatter mode, target it's assigned corner
-//  1.2 if it's in chase mode, depending on the ghost, target the player or a specific position
-//  1.3 if it's in frightened mode, target a random position
-//  1.4 if it's in dead mode, target the ghost house
+// Checks if the ghost is at an intersection and changes direction
+// depending on the ghost's attribute
 void ghostPathing(struct Ghost* ghost, char** map, struct Player* player);
 
 #endif
