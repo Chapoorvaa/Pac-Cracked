32e0c3f0a02a38caaafe9be2358cf545695c28de
Thomas Polo <thomas@polo.eu.com>
2023-05-21 20:39:40 +0200

    added maps to maps.txt

--- maps/mapgen.h
+++ maps/mapgen.h
@@ -1,58 +1,23 @@
 #ifndef MAPGEN_H
 #define MAPGEN_H
 
+#define ROW 29
+#define COL 27
+
 #include "../pacman_ai/lib/linked_list.h"
 
-#define UP 0
-#define RIGHT 1
-#define DOWN 2
-#define LEFT 3
+typedef struct Dict {
+    struct Index* head;
+} Dict;
 
-#define num_rows 9
-#define num_cols 5
+struct Index {
+    struct Index* next;
+    char* key;
+    llist* value;
+};
 
-#define ghost_row 3
+Dict* learn_mapping(char* path);
 
-typedef struct Cell {
-    size_t x;
-    size_t y;
-    int filled;
-    int connect[4];
-    struct Cell** next;
-    int num;
-    int group;
-} Cell ;
-
-Cell** init_table(); 
-
-void set_ghost_home(Cell** table); 
-
-llist* get_leftmost_empty_cells(Cell** table);
-
-typedef struct State {
-    Cell** table;
-    struct Cell* cell;
-    struct Cell* first_cell;
-    int num_filled;
-    int num_groups;
-    int size;
-    int single_count;
-} State ;
-
-State* init_state();
-
-void fill_cell(State* state, struct Cell* cell);
-
-int rand_int(int min, int max);
-
-void *rand_element(llist* list);
-
-#define prob_top_and_bot 1
-
-int try_single_cell_group(State* state);
-
-struct Cell* start_new_group(State* state);
-
-State* gen_random_cells();
+char* generate_map(Dict* dictionary);
 
 #endif
